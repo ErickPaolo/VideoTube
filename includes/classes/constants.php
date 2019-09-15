5937c2b5d2d9bab4df4620007eb89ca338059df6
ErickPaolo <zapata26erick@gmail.com>
2019-09-14 21:47:18 -0500

    Agregado de Assets / Includes / Uploads

---
+++ includes/classes/constants.php
@@ -0,0 +1,14 @@
+<?php 
+class Constants{
+    public static $firstNameCharacters = "Tu primer nombre debe estar entre 2 y 25 caracteres.";
+    public static $lastNameCharacters = "Tu segundo nombre debe estar entre 2 y 25 caracteres.";
+    public static $usernameCharacters = "Tu usuario debe estar entre 5 y 25 caracteres.";
+    public static $usernameTaken = "Este usuario ya esta en uso. Elige otro.";
+    public static $emailsDoNotMatch = "Los correos no coinciden. Vuelve a intentarlo.";
+    public static $emailInvalid = "Esta dirección de correo electrónico no es válida.";
+    public static $emailTaken = "Este correo electrónico ya esta en uso. Elige otro.";
+    public static $passwordsDoNotMatch = "Las contraseñas no coinciden. Vuelve a intentarlo.";
+    public static $passwordNotAlphanumeric = "La contraseña solo puede tener letras y números.";
+    public static $passwordLength = "La contraseña debe tener al menos 6 caracteres.";
+}
+?>
